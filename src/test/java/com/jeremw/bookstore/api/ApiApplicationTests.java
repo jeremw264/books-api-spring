75de1aebe0ac2485b5a4bcb8328727003455d58e
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-05-11 14:16:14 +0200

    🎉 Begin of project

---
+++ src/test/java/com/jeremw/bookstore/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremw.bookstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
